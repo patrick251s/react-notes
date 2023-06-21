e7494e2fc2855ec665e064bff649e2b875099d86
patrick251s <patryk.kazmierak99@gmail.com>
2023-06-21 19:07:22 +0200

    change header

--- src/InputSection.js
+++ src/InputSection.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InputSection.css';
 
 function InputSection() {
 
+  //const [header, setHeader] = useState('Hola Español!');
+
   const spanishFlagClick = () => {
     // Kod do wykonania po kliknięciu
     console.log('Flaga Hiszpanii została kliknięta!');
+    //setHeader('Hola Español!')
   };
 
   const englandFlagClick = () => {
     // Kod do wykonania po kliknięciu
     console.log('Flaga Wielkiej Brytanii została kliknięta!');
+    //setHeader('Hello English!')
   };
 
     return (
-      <form>
-        <div className="row pb-4">
-            <div className="col-sm-5 mx-auto mb-3 mb-sm-0">
-                <div id="flagDiv" className="row mb-2">
-                  <button id="polish" type="button" className="col-2 p-0 m-0 mx-auto"><img src="polska.jpg" className="col-12" alt="Polish flag"/></button>
+        <main className="mx-auto">
+          <h2 id="head" className="text-primary py-3">{header}</h2>
+          <form>
+            <div className="row pb-4">
+                <div className="col-sm-4 mx-auto mb-3 mb-sm-0">
+                    <div id="flagDiv" className="row mb-2">
+                      <button id="polish" type="button" className="col-2 p-0 m-0 mx-auto"><img src="polska.jpg" className="col-12" alt="Polish flag"/></button>
+                    </div>
+                    <textarea id="polishArea" className="col-12"></textarea>
                 </div>
-                <textarea id="polishArea" className="col-12"></textarea>
-            </div>
-            <div className="col-sm-5 mx-auto">
-                <div id="flagDiv" class="row mb-2">
-                  <button onClick={spanishFlagClick} id="spain" type="button" className="col-2 p-0 m-0 mx-auto"><img src="spain.png" className="col-12" alt="Spanish flag"/></button>
-                  <button onClick={englandFlagClick} id="england" type="button" className="col-2 p-0 m-0 mx-auto"><img src="british.jpg" className="col-12" alt="English flag"/></button>
+                <div className="col-sm-4 mx-auto">
+                    <div id="flagDiv" className="row mb-2">
+                      <button onClick={spanishFlagClick} id="spain" type="button" className="col-2 p-0 m-0 mx-auto"><img src="spain.png" className="col-12" alt="Spanish flag"/></button>
+                      <button onClick={englandFlagClick} id="england" type="button" className="col-2 p-0 m-0 mx-auto"><img src="british.jpg" className="col-12" alt="English flag"/></button>
+                    </div>
+                    <textarea id="foreignArea" className="col-12"></textarea>
                 </div>
-                <textarea id="foreignArea" className="col-12"></textarea>
+                
             </div>
-            
-        </div>
-        <input type="submit" className="btn btn-success col-6 col-sm-2 mx-auto" value="Zapisz"/>
-      </form>
+            <input type="submit" className="btn btn-success col-6 col-sm-2 mx-auto" value="Zapisz"/>
+          </form>
+        </main>
       
     )
   }
